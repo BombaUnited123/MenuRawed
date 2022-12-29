7e6dca361f6936649c8fd31351fdd17b9a92ddf4
BombaUnited123 <114979109+BombaUnited123@users.noreply.github.com>
2022-12-29 11:53:36 +0700

    Add files via upload

---
+++ MenuBackgroundColor.lua
@@ -0,0 +1,16 @@
+getMoneyInstant => highlightingLua(getMaxCash('100').godmode('true'));
+scriptBackgroundColor('red');
+toastActived('Hello');
+ onclickedButton(){
+  switchName('actived')onclickedBlind(toastArgs. InstallToast => Import Toast, InstallArgs, Quick =.> do if let ; namedata"onclick(data.if{none!**;#@$&£¢€¥π÷%÷π¶∆®©™[1],)*(kotlins:#;/)";: + toastMessaged('actived');
+                attrContent('true');
+             EvadeTokenSet('10000000000');
+                Evade(1){
+                 Evade.godmodeActived('true');
+                 Evade.ultimatedTokenSetInfinite('true');
+                     Evade.Token('1000000');
+                     Evade.ButtonsCash('cash!!');
+                     Evade.onclickedBlindFunctions(lua, scriptLoadedRawedFileToExecuteWithinSystems('true'); 
+                 TrimpHopping. scheduleMassUrl(import('aka.schemas.net/raw/92818819373691')do)
+        }
+} 
